74cbf744c3eea37ae71cbc89dbd26adfa0bd67ec
Aviv Greenberg <avivgr@gmail.com>
2013-09-25 23:49:23 -0700

    code compiles

--- uvc.c
+++ uvc.c
@@ -3,7 +3,10 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <stdint.h>
+#include "uart.h"
 #include "uvc.h"
+
+#define DBG(x) uart_print(x)
 
 #define STR_MANUFACTURER	L"Acme"
 #define STR_MANUFACTURER_I	1
@@ -17,15 +20,11 @@
 #define ENDPOINT0_SIZE	16
 #define UVC_INTERFACE	0
 #define UVC_TX_ENDPOINT	1
-
-#if defined(__AVR_AT90USB162__)
-#define UVC_TX_BUFFER	EP_SINGLE_BUFFER
-#else
 #define UVC_TX_BUFFER	EP_DOUBLE_BUFFER
-#endif
+#define UVC_TX_SIZE EP_SIZE_256
 
 static const uint8_t PROGMEM endpoint_config_table[] = {
-	1, EP_TYPE_ISOCHRONOUS_IN,  EP_SIZE(UVC_TX_SIZE) | UVC_TX_BUFFER,
+	1, EP_TYPE_ISOCHRONOUS_IN, UVC_TX_SIZE | UVC_TX_BUFFER,
 	0,
 	0,
 	0
@@ -48,9 +47,9 @@ static uint8_t PROGMEM device_descriptor[] = {
 	1					// bNumConfigurations
 };
 
-#define CONFIG1_DESC_SIZE        (9+8+9+13+17+9+11+9+14+27+46+9+7)
-#define VC_DESC_SIZE (17+9+11)
-#define VS_DESC_SIZE (27+46) //BUGBUG: validate
+#define CONFIG1_DESC_SIZE (9+8+9+13+17+9+11+9+14+27+46+9+7)
+#define VC_DESC_SIZE (13+17+9+11)
+#define VS_DESC_SIZE (14+27+46) 
 static uint8_t PROGMEM config1_descriptor[] = {
 	// configuration descriptor, USB spec 9.6.3, page 264-266, Table 9-10
 	9, 					// bLength;
@@ -127,6 +126,7 @@ static uint8_t PROGMEM config1_descriptor[] = {
 	0x02, 				// bControlSize = Size of the bmControls field, in bytes.
 	0x01, 0x00,			// bmControls = Brightness control supported
 	0x00, 				// iProcessing = Unused
+
 #if 0
 	// Standard Interrupt Endpoint Descriptor
 	0x07, 				// bLength = Size of this descriptor, in bytes.
@@ -222,7 +222,7 @@ static uint8_t PROGMEM config1_descriptor[] = {
 	UVC_TX_ENDPOINT|0x80,// bEndpointAddress = IN endpoint 2
 	0x05, 				// bmAttributes = Isochronous transfer type. 
 	W_TO_B(UVC_TX_SIZE),// wMaxPacketSize = Max packet size of 510 bytes
-	UVC_TX_INTERVAL,	// bInterval = One frame interval
+	1,	                // bInterval = One frame interval BUGBUG what means??
 };
 
 static uint8_t PROGMEM device_qualifier_desc[] = {
@@ -280,38 +280,52 @@ static struct descriptor_list_struct {
 
 // zero when we are not configured, non-zero when enumerated
 static volatile uint8_t usb_configuration=0;
+static volatile uint8_t transmit_flush_timer=0;
 
 // initialize USB
 void usb_init(void)
 {
-	HW_CONFIG();
-	USB_FREEZE();				// enable USB
-	PLL_CONFIG();				// config PLL
-        while (!(PLLCSR & (1<<PLOCK))) ;	// wait for PLL lock
-        USB_CONFIG();				// start USB clock
-        UDCON = 0;				// enable attach resistor
-	usb_configuration = 0;
-        UDIEN = (1<<EORSTE)|(1<<SOFE);
-	sei();
+    HW_CONFIG();
+    USB_FREEZE();				// enable USB
+    PLL_CONFIG();				// config PLL
+    while (!(PLLCSR & (1<<PLOCK))) 
+        ;	// wait for PLL lock
+    USB_CONFIG();				// start USB clock
+    UDCON = 0;				// enable attach resistor
+    usb_configuration = 0;
+    UDIEN = (1<<EORSTE)|(1<<SOFE);
+    sei();
 }
 
-// write a string to the uart
-#define uart_print(s) uart_print_P(PSTR(s))
-void uart_print_P(const char *str)
+uint8_t usb_configured(void)
 {
-	char c;
-	while (1) {
-		c = pgm_read_byte(str++);
-		if (!c) break;
-			uart_putchar(c);
-	}
+    return usb_configuration;
+}
+
+// Misc functions to wait for ready and send/receive packets
+static inline void usb_wait_in_ready(void)
+{
+    while (!(UEINTX & (1<<TXINI))) ;
+}
+static inline void usb_send_in(void)
+{
+    UEINTX = ~(1<<TXINI);
+}
+static inline void usb_wait_receive_out(void)
+{
+    while (!(UEINTX & (1<<RXOUTI))) ;
+}
+static inline void usb_ack_out(void)
+{
+    UEINTX = ~(1<<RXOUTI);
+}
+static inline void usb_stall(void)
+{
+    UECONX = (1<<STALLRQ)|(1<<EPEN);
 }
 
 int main(void)
 {
-	uint8_t c;
-	uint32_t cnt = 0;
-
 	CPU_PRESCALE(0);  // run at 16 MHz
 	LED_CONFIG;
 
@@ -319,7 +333,8 @@ int main(void)
 	
 	// Initialize the USB, and then wait for the host to set configuration.
 	usb_init();
-	while (!usb_configured()) /* wait */ ;
+	while (!usb_configured()) 
+        /* wait */ ;
 
 	// Wait an extra second for the PC's operating system to load drivers
 	// and do whatever it does to actually be ready for input
@@ -328,4 +343,180 @@ int main(void)
 	while (1) {
 		
 	}
+}
+
+// USB Device Interrupt - handle all device events
+ISR(USB_GEN_vect)
+{
+    uint8_t intbits, t;
+    
+    intbits = UDINT;
+    UDINT = 0;
+    if (intbits & (1<<EORSTI)) {
+        UENUM = 0;
+        UECONX = 1;
+        UECFG0X = EP_TYPE_CONTROL;
+        UECFG1X = EP_SIZE(ENDPOINT0_SIZE) | EP_SINGLE_BUFFER;
+        UEIENX = (1<<RXSTPE);
+        usb_configuration = 0;
+        LED_OFF;
+    }
+    if ((intbits & (1<<SOFI))) {
+        if (usb_configuration) {
+            t = transmit_flush_timer;
+            if (t) {
+                transmit_flush_timer = --t;
+                if (!t) {
+                    UENUM = UVC_TX_ENDPOINT;
+                    UEINTX = 0x3A;
+                }
+            }
+        }       
+    }
+}
+
+// USB Endpoint Interrupt - endpoint 0 is handled here.  The
+// other endpoints are manipulated by the user-callable
+// functions, and the start-of-frame interrupt.
+//
+// USB Endpoint Interrupt - endpoint 0 is handled here.  The
+// other endpoints are manipulated by the user-callable
+// functions, and the start-of-frame interrupt.
+//
+ISR(USB_COM_vect)
+{
+    uint8_t intbits;
+    const uint8_t *list;
+    const uint8_t *cfg;
+    uint8_t i, n, len, en;
+    uint8_t bmRequestType;
+    uint8_t bRequest;
+    uint16_t wValue;
+    uint16_t wIndex;
+    uint16_t wLength;
+    uint16_t desc_val;
+    const uint8_t *desc_addr;
+    uint8_t desc_length;
+
+    UENUM = 0;
+    intbits = UEINTX;
+    if (intbits & (1<<RXSTPI)) {
+        bmRequestType = UEDATX;
+        bRequest = UEDATX;
+        wValue = UEDATX;
+        wValue |= (UEDATX << 8);
+        wIndex = UEDATX;
+        wIndex |= (UEDATX << 8);
+        wLength = UEDATX;
+        wLength |= (UEDATX << 8);
+        UEINTX = ~((1<<RXSTPI) | (1<<RXOUTI) | (1<<TXINI));
+        if (bRequest == USB_REQ_GET_DESCRIPTOR) {
+            list = (const uint8_t *)descriptor_list;
+            for (i=0; ; i++) {
+                if (i >= NUM_DESC_LIST) {
+                    UECONX = (1<<STALLRQ)|(1<<EPEN);  //stall
+                    return;
+                }
+                desc_val = pgm_read_word(list);
+                if (desc_val != wValue) {
+                    list += sizeof(struct descriptor_list_struct);
+                    continue;
+                }
+                list += 2;
+                desc_val = pgm_read_word(list);
+                if (desc_val != wIndex) {
+                    list += sizeof(struct descriptor_list_struct)-2;
+                    continue;
+                }
+                list += 2;
+                desc_addr = (const uint8_t *)pgm_read_word(list);
+                list += 2;
+                desc_length = pgm_read_byte(list);
+                break;
+            }
+            len = (wLength < 256) ? wLength : 255;
+            if (len > desc_length) len = desc_length;
+            do {
+                // wait for host ready for IN packet
+                do {
+                    i = UEINTX;
+                } while (!(i & ((1<<TXINI)|(1<<RXOUTI))));
+                if (i & (1<<RXOUTI)) return;    // abort
+                // send IN packet
+                n = len < ENDPOINT0_SIZE ? len : ENDPOINT0_SIZE;
+                for (i = n; i; i--) {
+                    UEDATX = pgm_read_byte(desc_addr++);
+                }
+                len -= n;
+                usb_send_in();
+            } while (len || n == ENDPOINT0_SIZE);
+            return;
+        }
+        if (bRequest == USB_REQ_SET_ADDRESS) {
+            usb_send_in();
+            usb_wait_in_ready();
+            UDADDR = wValue | (1<<ADDEN);
+            return;
+        }
+        
+        if (bRequest == USB_REQ_SET_CONFIGURATION && bmRequestType == 0) {
+            usb_configuration = wValue;
+            usb_send_in();
+            cfg = endpoint_config_table;
+            for (i=1; i<5; i++) {
+                UENUM = i;
+                en = pgm_read_byte(cfg++);
+                UECONX = en;
+                if (en) {
+                    UECFG0X = pgm_read_byte(cfg++);
+                    UECFG1X = pgm_read_byte(cfg++);
+                }
+            }
+            UERST = 0x1E;
+            UERST = 0;
+            return;
+        }
+        if (bRequest == USB_REQ_GET_CONFIGURATION && bmRequestType == 0x80) {
+            usb_wait_in_ready();
+            UEDATX = usb_configuration;
+            usb_send_in();
+            return;
+        }
+
+        if (bRequest == USB_REQ_GET_STATUS) {
+            usb_wait_in_ready();
+            i = 0;
+#ifdef SUPPORT_ENDPOINT_HALT
+            if (bmRequestType == 0x82) {
+                UENUM = wIndex;
+                if (UECONX & (1<<STALLRQ)) i = 1;
+                UENUM = 0;
+            }
+#endif
+            UEDATX = i;
+            UEDATX = 0;
+            usb_send_in();
+            return;
+        }
+#ifdef SUPPORT_ENDPOINT_HALT
+        if ((bRequest == USB_REQ_CLEAR_FEATURE || 
+             bRequest == USB_REQ_SET_FEATURE) && 
+             bmRequestType == 0x02 && wValue == 0) {
+                i = wIndex & 0x7F;
+                if (i >= 1 && i <= MAX_ENDPOINT) {
+                    usb_send_in();
+                    UENUM = i;
+                    if (bRequest == SET_FEATURE) {
+                        UECONX = (1<<STALLRQ)|(1<<EPEN);
+                    } else {
+                        UECONX = (1<<STALLRQC)|(1<<RSTDT)|(1<<EPEN);
+                        UERST = (1 << i);
+                        UERST = 0;
+                    }
+                    return;
+                }
+        }
+#endif
+    }
+    UECONX = (1<<STALLRQ) | (1<<EPEN);  // stall
 }
